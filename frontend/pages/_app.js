1b96f993d57e8825dccc1e0d1156916eac7c18c5
prestarapi30-ctrl <prestarapi30@gmail.com>
2025-11-11 04:03:24 -0500

    Add files via upload

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,16 @@
 import '../styles/global.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="icon" href="/LOGO.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/LOGO.png" />
+        <meta name="theme-color" content="#111827" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
